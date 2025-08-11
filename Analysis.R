f69737194ead55f47b731b96b16601a89eb48676
Karim Mehrab <mkarim@mrc.gm>
2025-08-11 14:07:44 +0000

    sfsd

--- Analysis.R
+++ Analysis.R
@@ -1,19 +1,3 @@
 add_op = function (x,y){
   return(x+y)
 }
-<<<<<<< HEAD
-
-mul_op <- function (x,y){
-  return(x*y)
-}
-
-add_op(10,20)
-mul_op(10,23)
-
-natto_op = function(){
-  print("totto")
-}
-
-natto_op()
-=======
->>>>>>> parent of f76a2c3 (Added the mul op)
